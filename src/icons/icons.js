3fff612e03678cd5ebab0f96d61f5ed8c9f69f4a
busrakk <busrakartal160@gmail.com>
2023-06-06 03:12:33 +0300

    define color and icon constants

---
+++ src/icons/icons.js
@@ -0,0 +1,35 @@
+import { CgMenuGridR } from "react-icons/cg";
+import { IoMdSearch, IoMdNotifications, IoMdSettings } from "react-icons/io";
+import {
+  MdMessage,
+  MdPeopleAlt,
+  MdOutlineModeComment,
+  MdPermMedia,
+} from "react-icons/md";
+import {
+  BiBarChartSquare,
+  BiDotsHorizontalRounded,
+  BiDotsVerticalRounded,
+} from "react-icons/bi";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { AiFillFile, AiOutlinePlusCircle } from "react-icons/ai";
+import { TfiMenuAlt } from "react-icons/tfi";
+import { BsFillBookmarkFill, BsChatLeftTextFill } from "react-icons/bs";
+
+export const MENU_ICON = CgMenuGridR;
+export const SEARCH_ICON = IoMdSearch;
+export const NOTIFICATIONS_ICON = IoMdNotifications;
+export const MESSAGE_ICON = MdMessage;
+export const PEOPLE_ICON = MdPeopleAlt;
+export const BAR_CHART_ICON = BiBarChartSquare;
+export const DASHBOARD_ICON = LuLayoutDashboard;
+export const FILE_ICON = AiFillFile;
+export const POSTS_ICON = MdOutlineModeComment;
+export const MEDIA_ICON = MdPermMedia;
+export const CATEGORİES_ICON = TfiMenuAlt;
+export const TAGS_ICON = BsFillBookmarkFill;
+export const COMMENTS_ICON = BsChatLeftTextFill;
+export const SETTING_ICON = IoMdSettings;
+export const PLUS_ICON = AiOutlinePlusCircle;
+export const DOT_H_ICON = BiDotsHorizontalRounded;
+export const DOT_V_ICON = BiDotsVerticalRounded;
